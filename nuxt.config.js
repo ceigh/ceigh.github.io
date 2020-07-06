be016df13d3a324069bed5e0cb94ca4d6efcdd49
Artjom Löbsack <ceigh@pm.me>
2020-07-06 15:06:19 +0500

    :mag: add name as title

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,7 @@ export default {
   mode: 'universal',
   target: 'server',
   head: {
+    title: 'Artjom Löbsack',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' }
