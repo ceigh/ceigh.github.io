0e8fa89cb048cb91bf4a133875ecf3ac48468e14
Artjom Löbsack <ceigh@pm.me>
2020-07-06 17:06:45 +0500

    :bento: add favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,6 +6,9 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/png', href: '/favicon.png' }
     ]
   },
   css: [
