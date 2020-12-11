690aba305a7ecf9cd053735d3765d0acb5833b84
Artjom Löbsack <me@ceigh.com>
2020-12-11 18:49:52 +0500

    feat(index): add troxler

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,30 @@
 <template lang='pug'>
 div
-  h2 Me
+  h2 About me
   NuxtContent(:document='index')
+  br
+  figure
+    img(:src='getRandomTroxler()'
+      alt='troxler')
+    figcaption Troxler's fading
 </template>
 
 <script>
+
 export default {
   layout: 'header',
 
   async asyncData ({ $content }) {
     return { index: await $content('index').fetch() }
+  },
+
+  methods: {
+    getRandomTroxler () {
+      const dir = '/images/troxler/'
+      const max = 11
+      const idx = Math.floor(Math.random() * (max - 1) + 1)
+      return `${dir}${idx}.jpg`
+    }
   },
 
   head: {
