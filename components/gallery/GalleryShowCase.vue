a454ffabeb1f6f5c54cb965ab0561ad223b83c40
Artjom Löbsack <me@ceigh.com>
2021-04-18 03:39:14 +0500

    chore(showcase): change image to img

--- components/gallery/GalleryShowCase.vue
+++ components/gallery/GalleryShowCase.vue
@@ -1,9 +1,9 @@
 <template lang='pug'>
 div
-  figure(v-for='(image, i) in images' :key='image.title'
+  figure(v-for='(img, i) in images' :key='img.title'
     :class='{ "white-bg": whiteBg, "mb-1": i !== images.length - 1 }')
-    img(:src='`/images/${image.src}`' :alt='image.name')
-    figcaption {{ image.name }}
+    img(:src='`/images/${img.src}`' :alt='img.name')
+    figcaption {{ img.name }}
 </template>
 
 <script>
