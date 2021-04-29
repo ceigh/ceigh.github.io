ae924569368e2d7cb01b78e259b832a7a4d33126
Artjom Löbsack <me@ceigh.com>
2021-04-30 04:16:11 +0500

    refactor(hello): use const z on light

--- components/demo/hello/three/light.ts
+++ components/demo/hello/three/light.ts
@@ -1,5 +1,6 @@
 import * as T from 'three'
 
+const z = 2
 let xCoef: number
 let yCoef: number
 let camera: T.Camera
@@ -9,13 +10,13 @@ function onMove (e: PointerEvent): void {
   const x = xCoef * e.clientX - 1
   const y = yCoef * e.clientY + 1
 
-  const vec = new T.Vector3(x, y, 2)
+  const vec = new T.Vector3(x, y, z)
   vec.unproject(camera)
   const dir = vec.sub(camera.position).normalize()
   const dis = -camera.position.z / dir.z
   const pos = camera.position.clone().add(dir.multiplyScalar(dis))
 
-  light.position.set(pos.x, pos.y, 2)
+  light.position.set(pos.x, pos.y, z)
 }
 
 export function getBulb (w: number, h: number, c: T.Camera): T.PointLight {
@@ -28,7 +29,7 @@ export function getBulb (w: number, h: number, c: T.Camera): T.PointLight {
   const mesh = new T.Mesh(geometry, material)
 
   light = new T.PointLight(0xFFFFFF, 1, 20, 2)
-  light.position.set(0, 0, 2)
+  light.position.set(0, 0, z)
   light.add(mesh)
 
   window.addEventListener('pointermove', onMove)
