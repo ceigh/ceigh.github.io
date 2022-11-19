57ddce7f64d16fdc0b522794c888d9d3a739dc5d
Hecig <me@ceigh.com>
2022-11-19 18:14:39 +0500

    feat: init

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+  },
+
+  extends: [
+    "standard",
+    "plugin:vue/vue3-recommended",
+  ],
+
+  rules: {
+    quotes: ["warn", "double"],
+    "comma-dangle": ["warn", "always-multiline"],
+    "no-alert": "warn",
+    "no-console": ["warn", { allow: ["warn", "error"] }],
+    "prefer-template": "warn",
+    "multiline-comment-style": ["warn", "separate-lines"],
+  },
+}
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { createApp } from "vue"
+import "modern-normalize"
+import "./assets/style/index.css"
+import App from "./App.vue"
+
+createApp(App).mount("#app")
