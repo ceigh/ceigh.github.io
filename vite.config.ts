a9b12fd6f11b0da1536d733bd8adaee795bc1b29
Hecig <me@ceigh.com>
2022-11-26 14:14:44 +0500

    chore: use 0.0.0.0:3000 for dev and preview

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,13 @@ import components from "unplugin-vue-components/vite"
 import iconsResolver from "unplugin-icons/resolver"
 import icons from "unplugin-icons/vite"
 
+const server = { host: "0.0.0.0", port: 3000 }
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  server,
+  preview: server,
+
   plugins: [
     vue(),
 
