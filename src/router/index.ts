11957ba9ebcb419ff9f1176094e3305da4d41015
Hecig <me@ceigh.com>
2022-12-21 20:27:33 +0500

    fix: use web hash history

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router"
+import { createRouter, createWebHashHistory } from "vue-router"
 import { items } from "../items"
 
 const capitalize = (str: string) => `${str[0].toUpperCase()}${str.slice(1)}`
@@ -17,7 +17,7 @@ const routes = [
 ]
 
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
 
