0da4e0a962fbf2b20179f87e609df22a17f1c49b
ceigh <ceigh@users.noreply.github.com>
2023-07-30 11:13:11 +0000

    deploy: 4bf38334b7c1dc60027aa8ad9a2bfb1033f1c0b9

--- assets/not-found.13cb74e9.js
+++
@@ -1,1 +0,0 @@
-import{d as e,o,c as t,u as r}from"./index.1c0582b1.js";const p=e({__name:"not-found",setup(n){return r().push("/"),(u,a)=>(o(),t("div"))}});export{p as default};
---
+++ assets/not-found.5e92e82f.js
@@ -0,0 +1,1 @@
+import{d as e,u as o,o as t,c as r}from"./index.be31f87b.js";const p=e({__name:"not-found",setup(n){return o().push("/"),(u,a)=>(t(),r("div"))}});export{p as default};
