88fdc55a63120f2797a3f41b84d27a06b11a5caa
Артем Лебзак <giceh@ya.ru>
2025-06-07 23:07:40 +0500

    chore: cleanup

--- .stylelintrc.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: [
-    "stylelint-config-standard",
-    "stylelint-config-rational-order",
-    "stylelint-config-recommended-vue",
-  ],
-}
--- src/router/pages/items/hello/three/scene.ts
+++
@@ -1,7 +0,0 @@
-import * as T from "three"
-
-export function getScene (): T.Scene {
-  const scene = new T.Scene()
-
-  return scene
-}
