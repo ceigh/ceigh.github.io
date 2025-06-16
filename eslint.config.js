dd840d01dd3ecfabceff0bb1d91c861e1e0f06e7
Артем Лебзак <giceh@ya.ru>
2025-06-16 20:19:16 +0500

    perf: partially use oxlint for linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 import antfu from '@antfu/eslint-config'
+import oxlint from 'eslint-plugin-oxlint'
 
 export default antfu(
   {
@@ -51,4 +52,6 @@ export default antfu(
       }],
     },
   },
+
+  ...oxlint.buildFromOxlintConfigFile('./.oxlintrc.json'),
 )
