3a84154093866bca61d3f5c553eb84bf8481e156
Артем Лебзак <giceh@ya.ru>
2025-06-24 20:30:38 +0500

    feat: add markings

--- layouts/default.vue
+++ layouts/default.vue
@@ -24,13 +24,26 @@
           </div>
         </div>
 
-        <div class="p-14 p-t-7 grow-1 overflow-hidden">
+        <div class="p-x-14 p-y-7 grow-1 overflow-hidden">
           <div class="text-3xl font-mono p-2px rounded-8px h-full overflow-hidden bg-screen-border shadow-screen-border shadow-screen-glow">
             <div class="border-0.5px border-slate-500 rounded-6px bg-blue-100 h-full overflow-hidden bg-screen-grid">
               <div class="p-6 h-full overflow-y-auto">
                 <slot />
               </div>
             </div>
+          </div>
+        </div>
+
+        <div class="text-gray-600 p-x-14 p-b-7 flex flex-col gap-2 max-w-fit items-end">
+          <div class="text-4xl flex gap-2">
+            <i class="i-app:cc" />
+            <i class="i-app:accessibility" />
+            <i class="i-app:open-source" />
+            <i class="i-app:no-ai" />
+          </div>
+
+          <div class="text-0.5rem uppercase">
+            2025 Made in Russia
           </div>
         </div>
       </div>
